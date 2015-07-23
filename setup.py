3344a501f99feb39bdc6ca97a41f95d8f5c30d37
Oleg Tarasenko <oltarasenko@gmail.com>
2015-07-23 14:30:12 +0300

    updated setup

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='toggl-indicator',
     version='1.0',
+    entry_points = {
+        'console_scripts' : ['myscript = toggl.toggl_indicator:main']
+    },
     packages=find_packages()
 )
 
